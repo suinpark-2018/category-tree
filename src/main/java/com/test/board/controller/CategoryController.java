bd01d2e1588eabd4e05d05933b8b4fe0a3fb5880
JJackSparrow <parksuuuun@gmail.com>
2024-07-02 19:58:16 +0900

    [Feat] 카테고리 Controller 구현
    HomeController, CategoryController 구현 완료

---
+++ src/main/java/com/test/board/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package com.test.board.controller;
+
+import com.test.board.domain.CategoryDto;
+import com.test.board.service.CategoryServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/category")
+public class CategoryController {
+    private CategoryServiceImpl categoryService;
+
+    @Autowired
+    public CategoryController(CategoryServiceImpl categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public List<CategoryDto> list(int cno, String name) throws Exception {
+        List<CategoryDto> categories = categoryService.getCategoryWithChildren(cno, name);
+        return categories;
+    }
+}
