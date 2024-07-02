9e0303d0ce081fc129bf1086ee5f953350460332
JJackSparrow <parksuuuun@gmail.com>
2024-07-02 19:59:46 +0900

    [Feat] Json 활용 위한 클래스 추가
    Json 활용 위해 JsonConfig 파일에 bean 등록 완료

---
+++ src/main/java/com/test/board/common/config/JsonConfig.java
@@ -0,0 +1,25 @@
+package com.test.board.common.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class JsonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        jsonConverter.setDefaultCharset(StandardCharsets.UTF_8);
+        jsonConverter.setObjectMapper(objectMapper());
+        return jsonConverter;
+    }
+}
