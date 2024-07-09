efc43108cb719820de3cd1537a103d99cbe10a2f
JJackSparrow <parksuuuun@gmail.com>
2024-07-09 12:39:44 +0900

    [Refactor] 불필요한 코드 삭제

--- src/main/java/com/test/board/dao/CategoryDaoImpl.java
+++ src/main/java/com/test/board/dao/CategoryDaoImpl.java
@@ -4,10 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.test.board.domain.CategoryDto;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public class CategoryDaoImpl implements CategoryDao {
